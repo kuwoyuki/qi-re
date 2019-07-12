156ec9bbae807d051491034200d4116e1df67868
kuwoyuki <kuwoyuki@cock.li>
2019-07-12 13:45:39 +0200

    edit

--- README.md
+++ README.md
@@ -19,23 +19,20 @@ My guess at `Java_a_b_b`
 
 ```java
 public static byte[] b(long bookId, long chapterId, byte[] bs, long userId, String imei) {
+    String salt1 = "" + userId + imei + chapterId + "2EEE1433A152E84B3756301D8FA3E69A";
+
     // First secret, calls s :: String -> String -> String
     // SHA1 HMAC?
-    String secret1 = s(
-      imei,
-      "" + userId + imei + chapterId + "2EEE1433A152E84B3756301D8FA3E69A"
-    );
+    String secret1 = s(imei, salt1);
 
     if (secret1.length() > 24) {
         secret1 = secret1.substring(0, 24);
     }
 
+    String salt2 = secret1 + imei
     // Second secret, calls m :: String -> String -> String
     // something MD5
-    String secret2 = m(
-      bookId + "",
-      secret1 + imei
-    );
+    String secret2 = m(userId + "", salt2);
 
     if (secret2.length() > 24) {
         secret2 = secret2.substring(0, 24);
--- shared/Java_a_b.java
+++ shared/Java_a_b.java
@@ -9,26 +9,7 @@ import com.qidian.QDReader.core.log.QDLog;
 import java.security.NoSuchAlgorithmException;
 
 public class b {
-    // public static native byte[] b(long j, long j2, byte[] bArr, long j3, String str);
-    public static byte[] b(int bookId, int chapterId, byte[] bs, long userId, String imei) {
-        String salt1 = "" + userId + imei + chapterId + "2EEE1433A152E84B3756301D8FA3E69A";
-        String secret1 = s(imei, salt1);
-
-        if (secret1.length() > 24) {
-            secret1 = secret1.substring(0, 24);
-        }
-
-        String salt2=secrect1+aid;
-        String secret2=m(uid + "", salt2);
-        
-        if (secret2.length() > 24) {
-            secret2 = secret2.substring(0, 24);
-        }
-        
-        bs = d(bs, secret2);
-        return d(bs,secrect1);
-    }
-
+    public static native byte[] b(long j, long j2, byte[] bArr, long j3, String str);
 
     public static native void c(Context context);
 
